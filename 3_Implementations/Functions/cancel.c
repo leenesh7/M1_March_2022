cfd3037e10ff03160033bb81ff30d312dd763461
leenesh7 <101555328+leenesh7@users.noreply.github.com>
2022-04-04 12:29:46 +0530

    Add files via upload

---
+++ 3_Implementations/Functions/cancel.c
@@ -0,0 +1,31 @@
+#include"Train_ticket_reservation.h"
+void cancel(void)   /* Sorry this function does not work. Coding is not completed. Codes have been removed due to some errors  */
+{
+	/*FILE *f,*t;
+	int i=0;
+	int trainnum;
+	t=fopen("seats_reserved.txt","w");
+	if ((f=fopen("seats_reserved.txt","r"))==NULL)
+	{
+		printf("NO RECORD ADDED.");
+		main();
+		* * * * *
+		*
+		*
+		*
+		*
+		*
+		*
+		*  /  missing codes  /
+	}
+	else*/
+	system("cls");
+	int trainnum;
+	printf("-----------------------\n");
+		printf("Enter the train number: \n");
+			printf("-----------------------\n");
+		fflush(stdin);
+		scanf("%i",&trainnum);
+		printf("\n\nCancelled");
+		getch();
+}
